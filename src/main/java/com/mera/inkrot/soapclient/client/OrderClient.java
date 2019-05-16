f75fef26c3589620fc4c8a34acbc7856fd951c47
Zaripov <izaripov@merann.ru>
2019-05-16 18:13:01 +0300

    All shell methods and OrderClient complete

--- src/main/java/com/mera/inkrot/soapclient/client/OrderClient.java
+++ src/main/java/com/mera/inkrot/soapclient/client/OrderClient.java
@@ -1,11 +1,66 @@
 package com.mera.inkrot.soapclient.client;
 
-import com.mera.inkrot.soapclient.wsdl.order.GetById;
-import com.mera.inkrot.soapclient.wsdl.order.GetByIdResponse;
+import com.mera.inkrot.soapclient.wsdl.order.*;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
-
 import javax.xml.bind.JAXBElement;
 import javax.xml.namespace.QName;
+import java.util.List;
+
+public class OrderClient extends WebServiceGatewaySupport {
+
+    public static final String NAMESPACE_URI = "http://service.carshowroom.inkrot.mera.com/";
+
+    @SuppressWarnings("unchecked")
+    public JAXBElement<?> getResponse(String methodName, Class declaredType, Object value) {
+        return ((JAXBElement<GetByIdResponse>) getWebServiceTemplate()
+                .marshalSendAndReceive(getDefaultUri(), new JAXBElement<>(new QName(NAMESPACE_URI, methodName), declaredType, value)));
+    }
+
+    @SuppressWarnings("unchecked")
+    public OrderDto saveOrder(OrderDto order) {
+        Save request = new Save();
+        request.setOrder(order);
+        return ((JAXBElement<SaveResponse>) getResponse("save", Save.class, request)).getValue().getOrder();
+    }
+
+    @SuppressWarnings("unchecked")
+    public OrderDto updateOrder(Long id, OrderDto order) {
+        Update request = new Update();
+        request.setId(id);
+        request.setOrder(order);
+        return ((JAXBElement<UpdateResponse>) getResponse("update", Update.class, request)).getValue().getOrder();
+    }
+
+    @SuppressWarnings("unchecked")
+    public String deleteOrder(Long id) {
+        Delete request = new Delete();
+        request.setId(id);
+        return ((JAXBElement<DeleteResponse>) getResponse("delete", Delete.class, request)).getValue().getReturn();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<OrderDto> getAllOrders() {
+        GetAll request = new GetAll();
+        return ((JAXBElement<GetAllResponse>) getResponse("getAll", GetAll.class, request)).getValue().getOrders().getOrder();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<OrderDto> getAllOrdersByStatusAndCustomer(StatusDto status, CustomerDto customer) {
+        GetAllByStatusAndCustomer request = new GetAllByStatusAndCustomer();
+        request.setStatus(status);
+        request.setCustomer(customer);
+        return ((JAXBElement<GetAllByStatusAndCustomerResponse>) getResponse("getAllByStatusAndCustomer", GetAllByStatusAndCustomer.class, request)).getValue().getOrders().getOrder();
+    }
+
+    @SuppressWarnings("unchecked")
+    public OrderDto getOrder(Long id) {
+        GetById request = new GetById();
+        request.setId(id);
+        return ((JAXBElement<GetByIdResponse>) getResponse("getById", GetById.class, request)).getValue().getOrder();
+    }
+}
+
+
 
 /*import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
@@ -14,28 +69,23 @@ import javax.xml.bind.Marshaller;
 import javax.xml.namespace.QName;
 import java.io.StringWriter;*/
 
-public class OrderClient extends WebServiceGatewaySupport {
+/*try {
+    GetById request = new GetById();
+    request.setId(id);
+    JAXBContext jaxbContext = JAXBContext.newInstance(GetById.class);
+    Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+    QName qName = new QName("http://service.carshowroom.inkrot.mera.com/", "getById");
+    JAXBElement<GetById> root = new JAXBElement<>(qName, GetById.class, request);
+    StringWriter stringWriter = new StringWriter();
+    jaxbMarshaller.marshal(root, stringWriter);
+    System.out.println(stringWriter.toString());
+} catch (JAXBException e) {
+}*/
 
-    public GetByIdResponse getOrder(Long id) {
-
-        /*try {
-            GetById request = new GetById();
-            request.setId(id);
-            JAXBContext jaxbContext = JAXBContext.newInstance(GetById.class);
-            Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-            jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            QName qName = new QName("http://service.carshowroom.inkrot.mera.com/", "getById");
-            JAXBElement<GetById> root = new JAXBElement<>(qName, GetById.class, request);
-            StringWriter stringWriter = new StringWriter();
-            jaxbMarshaller.marshal(root, stringWriter);
-            System.out.println(stringWriter.toString());
-        } catch (JAXBException e) {
-        }*/
-        GetById request = new GetById();
-        request.setId(id);
-        JAXBElement<GetByIdResponse> response = (JAXBElement<GetByIdResponse>) getWebServiceTemplate()
-                .marshalSendAndReceive("http://localhost:8080/soap/order", new JAXBElement<>(new QName("http://service.carshowroom.inkrot.mera.com/", "getById"),GetById.class,request));
-                        //, new SoapActionCallback("http://impl.service.carshowroom.inkrot.mera.com/OrderService"));
-        return response.getValue();
-    }
-}
+/*
+GetById request = new GetById();
+request.setId(id);
+JAXBElement<GetByIdResponse> response = (JAXBElement<GetByIdResponse>) getWebServiceTemplate()
+        .marshalSendAndReceive("http://localhost:8080/soap/order", new JAXBElement<>(new QName("http://service.carshowroom.inkrot.mera.com/", "getById"), GetById.class, request));
+                //, new SoapActionCallback("http://impl.service.carshowroom.inkrot.mera.com/OrderService"));*/
--- src/main/java/com/mera/inkrot/soapclient/command/ApplicationCommand.java
+++ src/main/java/com/mera/inkrot/soapclient/command/ApplicationCommand.java
@@ -1,78 +1,127 @@
 package com.mera.inkrot.soapclient.command;
 
 import com.mera.inkrot.soapclient.client.OrderClient;
-import com.mera.inkrot.soapclient.wsdl.order.GetByIdResponse;
+import com.mera.inkrot.soapclient.wsdl.order.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 @ShellComponent
 public class ApplicationCommand {
 
-    @Autowired
-    OrderClient orderClient;
+    private OrderClient orderClient;
 
-    // Example: get-order 1
-    @ShellMethod(key = "get-order", value = "Get order by id (test)")
-    public String getOrder(@ShellOption Long id) {
-        return orderClient.getOrder(id).getOrder().getBrandName();
+    @Autowired
+    public ApplicationCommand(OrderClient orderClient) {
+        this.orderClient = orderClient;
     }
 
-    /*
-    private Set<OptionDto> formatOptionsIdsArrayToSet(long[] optionsIds) {
-        Set<OptionDto> options = new HashSet<>();
-        for (long id : optionsIds)
-            options.add(new OptionDto(id));
+    private String toString(OrderDto order) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nЗаказ №")
+                .append(order.getId());
+        sb.append("\n\t- Клиент: ")
+                .append(order.getCustomer().getName())
+                .append("(id: ")
+                .append(order.getCustomer().getId())
+                .append(")");
+        sb.append("\n\t- Модель авто: ")
+                .append(order.getModelName());
+        sb.append("\n\t- Бренд авто: ")
+                .append(order.getBrandName());
+        sb.append("\n\t- Статус: ")
+                .append(order.getStatus().getName());
+        List<OptionDto> options = order.getOptions().getOption();
+        sb.append("\n\t- Опции: ").append(options.size() > 0 ? toStringOptions(options) : "-");
+        return sb.toString();
+    }
+
+    private String toStringOptions(List<OptionDto> options) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (OptionDto option : options)
+            sb.append(" ").append(option.getId()).append(" - ").append(option.getName()).append(",");
+        sb.deleteCharAt(sb.length() - 1).append(" ]");
+        return sb.toString();
+    }
+
+    private String toStringOrders(List<OrderDto> orders) {
+        StringBuilder sb = new StringBuilder();
+        for (OrderDto order : orders)
+            sb.append(toString(order)).append("\n");
+        return sb.toString();
+    }
+
+    private OrderDto formatStringsToObject(String customerName, String modelName, String brandName, Long statusId, long[] optionsIds) {
+        OrderDto order = new OrderDto();
+        CustomerDto customer = new CustomerDto();
+        customer.setName(customerName);
+        order.setCustomer(customer);
+        order.setModelName(modelName);
+        order.setBrandName(brandName);
+        StatusDto status = new StatusDto();
+        status.setId(statusId);
+        order.setStatus(status);
+        OrderDto.Options options = new OrderDto.Options();
+        List<OptionDto> optionsList = options.getOption();
+        optionsList.addAll(formatOptionsIdsArrayToList(optionsIds));
+        order.setOptions(options);
+        return order;
+    }
+
+    private List<OptionDto> formatOptionsIdsArrayToList(long[] optionsIds) {
+        List<OptionDto> options = new ArrayList<>();
+        for (long id : optionsIds) {
+            OptionDto option = new OptionDto();
+            option.setId(id);
+            options.add(option);
+        }
         return options;
     }
 
-    // Example: add-order --customer "Shell Customer" --model "Camry" --brand "Toyota" --options 1,2
+    // Example: add-order --customer "Soap Customer" --model "Camry" --brand "Toyota" --options 1,2
     @ShellMethod(key = "add-order", value = "Add order")
-    public String addOrder(@ShellOption String customer,
-                           @ShellOption String model,
-                           @ShellOption String brand,
+    public String addOrder(@ShellOption(value = "--customer") String customerName,
+                           @ShellOption(value = "--model") String modelName,
+                           @ShellOption(value = "--brand") String brandName,
                            @ShellOption(value = "--options", defaultValue = "") long[] optionsIds) {
-        OrderDto orderDto = new OrderDto(null, new CustomerDto(customer), model, brand, null, formatOptionsIdsArrayToSet(optionsIds));
-        return orderService.saveOrder(orderDto).toString();
+        OrderDto responseOrder = orderClient.saveOrder(formatStringsToObject(customerName, modelName, brandName, null, optionsIds));
+        return toString(responseOrder);
     }
 
     // Example: update-order --id 1 --customer "New"
-    @ShellMethod(key = "update-order", value = "Update order")
+    @ShellMethod(key = "update-order", value = "Update order by id")
     public String updateOrder(@ShellOption Long id,
-                              @ShellOption(defaultValue = "") String customer,
-                              @ShellOption(defaultValue = "") String model,
-                              @ShellOption(defaultValue = "") String brand,
+                              @ShellOption(value = "--customer", defaultValue = "") String customerName,
+                              @ShellOption(value = "--model", defaultValue = "") String modelName,
+                              @ShellOption(value = "--brand", defaultValue = "") String brandName,
                               @ShellOption(value = "--status", defaultValue = "-1") Long statusId,
                               @ShellOption(value = "--options", defaultValue = "") long[] optionsIds) {
-        if (customer.isEmpty()) customer = null;
-        if (model.isEmpty()) model = null;
-        if (brand.isEmpty()) brand = null;
+        if (customerName.isEmpty()) customerName = null;
+        if (modelName.isEmpty()) modelName = null;
+        if (brandName.isEmpty()) brandName = null;
         if (statusId == -1) statusId = null;
-        OrderDto orderDto = new OrderDto(id, new CustomerDto(customer), model, brand, new StatusDto(statusId, null), formatOptionsIdsArrayToSet(optionsIds));
-        //System.out.println(orderDto.toString());
-        return orderService.updateOrder(id, orderDto).toString();
+        OrderDto responseOrder = orderClient.updateOrder(id, formatStringsToObject(customerName, modelName, brandName, statusId, optionsIds));
+        return toString(responseOrder);
     }
 
     // Example: delete-order 50
-    @ShellMethod(key = "delete-order", value = "Delete order by id (test)")
+    @ShellMethod(key = "delete-order", value = "Delete order by id")
     public String deleteOrder(@ShellOption Long id) {
-        return orderService.deleteOrder(id);
+        return orderClient.deleteOrder(id);
     }
 
     // Example: get-all-orders
     @ShellMethod(key = "get-all-orders", value = "Get all orders")
     public String getAllOrders() {
-        List<OrderDto> orders = orderService.getAllOrders();
-        String ordersStr = orders.toString();
-        return ordersStr.substring(1, ordersStr.length() - 1);
+        return toStringOrders(orderClient.getAllOrders());
     }
 
-    // Example: get-all-orders-sc --sid 1 --scode "BEING_PROCESSED" --cid 1 --cname "New Customer"
+    // Example: get-all-orders-sc --sid 1 --cid 1 --cname "New Customer"
     @ShellMethod(key = "get-all-orders-sc", value = "Get all orders by status and customer")
     public String getAllOrdersByStatusAndCustomer(@ShellOption(value = "--sid", defaultValue = "-1") Long statusId,
                                                   @ShellOption(value = "--scode", defaultValue = "") String statusCode,
@@ -82,18 +131,19 @@ public class ApplicationCommand {
         if (statusCode.isEmpty()) statusCode = null;
         if (customerId == -1) customerId = null;
         if (customerName.isEmpty()) customerName = null;
-        StatusCustomerDto statusCustomerDto = new StatusCustomerDto(
-                new StatusDto(statusId, statusCode),
-                new CustomerDto(customerId, customerName)
-        );
-        List<OrderDto> orders = orderService.getAllOrdersByStatusAndCustomer(statusCustomerDto);
-        String ordersStr = orders.toString();
-        return ordersStr.substring(1, ordersStr.length() - 1);
+        StatusDto status = new StatusDto();
+        status.setId(statusId);
+        status.setCode(statusCode);
+        CustomerDto customer = new CustomerDto();
+        customer.setId(customerId);
+        customer.setName(customerName);
+        return toStringOrders(orderClient.getAllOrdersByStatusAndCustomer(status, customer));
     }
 
     // Example: get-order 1
     @ShellMethod(key = "get-order", value = "Get order by id (test)")
-    public OrderDto getOrder(@ShellOption Long id) {
-        return orderService.getOrder(id);
-    }*/
+    public String getOrder(@ShellOption Long id) {
+        OrderDto order = orderClient.getOrder(id);
+        return toString(order);
+    }
 }
